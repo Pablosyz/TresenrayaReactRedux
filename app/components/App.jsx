57774ff4ef8f7040c9d8ca466c04deb33692f459
ebarra <enrique.barra.arias@gmail.com>
2017-11-21 18:10:54 +0100

    Estado inicial y render

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import './../assets/scss/main.scss';
 
+const JUGADORX = "jugador 1 - las X";
+const JUGADOR0 = "jugador 2 - los 0";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        turno: JUGADORX,
+        valores: [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ],
+    };
+  }
+
   render() {
-    return (
-      <h2 id="heading">Hello ReactJS</h2>
-    );
+      let texto = "Turno del " + this.state.turno;
+      let htmltablero = this.state.valores.map(function (valoresFila, indiceFila) {
+        let fila = valoresFila.map(function (valor, indiceColumna) {
+          return (
+            <span>
+              {valor}
+            </span>
+          )
+        });
+        return (
+          <div>
+            {fila}
+          </div>
+        )
+      });
+
+      return (
+        <div>
+          <header className="cabecera">
+            {texto}
+          </header>
+          {htmltablero}
+        </div>
+      );
   }
 
 }
-
