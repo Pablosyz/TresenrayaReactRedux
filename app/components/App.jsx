07cf16c2d681285f49bdbd10c4859503641984d0
agordillo <agordillos@gmail.com>
2017-12-01 11:31:09 +0100

    New version of Tic Tac Toe implemented with React

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,62 +1,92 @@
 import React from 'react';
-import './../assets/scss/main.scss';
-import Cabecera from './Cabecera.jsx';
-import Tablero from './Tablero.jsx';
-import Reset from './Reset.jsx';
+import './../assets/css/main.css';
+import Header from './Header.jsx';
+import Board from './Board.jsx';
 
-const JUGADORX = "jugador 1 - las X";
-const JUGADOR0 = "jugador 2 - los 0";
+const PLAYER_X = "Player 1 (X)";
+const PLAYER_0 = "Player 2 (0)";
 
 export default class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-        turno: JUGADORX,
-        valores: [
+        turn: PLAYER_X,
+        winner: undefined,
+        values: [
         ['-', '-', '-'],
         ['-', '-', '-'],
         ['-', '-', '-'],
         ],
-        movimientos: 0
     };
-    this.appClick = this.appClick.bind(this);
-    this.resetClick = this.resetClick.bind(this);
+    this._onClick = this._onClick.bind(this);
   }
 
-  appClick(numeroFila, numberoColumna) {
-      let nuevosvalores = this.state.valores.slice();
-      let nuevoValor = this.state.turno === JUGADORX ? 'X' : '0';
-      nuevosvalores[numeroFila][numberoColumna] = nuevoValor;
-      this.setState({
-          turno: this.state.turno === JUGADORX ? JUGADOR0 : JUGADORX,
-          valores: this.state.valores,
-          movimientos: this.state.movimientos + 1
-      });
+  componentDidUpdate(){
+    if(typeof this.state.winner != "undefined"){
+      setTimeout(function(){
+        alert("The winner is " + this.state.winner);
+      }.bind(this),0);
+    }
   }
 
-  resetClick(){
+  _onClick(rowNumber,columnNumber){
+    if(typeof this.state.winner != "undefined"){
+      return;
+    }
+    var newValues = this.state.values.slice(); //Copy array to a new array (this method is valid for arrays of literal values)
+    var newValue = (this.state.turn === PLAYER_X ? 'X' : '0');
+    newValues[rowNumber][columnNumber] = newValue;
     this.setState({
-      turno: JUGADORX,
-      valores: [
-      ['-', '-', '-'],
-      ['-', '-', '-'],
-      ['-', '-', '-'],
-      ],
-      movimientos: 0
+      turn: (this.state.turn === PLAYER_X ? PLAYER_0 : PLAYER_X),
+      winner: this._getWinner(newValues),
+      values: newValues
     });
   }
 
-  render() {
-      let texto = "Turno del " + this.state.turno;
-
-      return (
-        <div>
-          <Cabecera texto={texto}/>
-          <Tablero valores={this.state.valores} appClick={this.appClick}/>
-          <h3>Número de movimientos: {this.state.movimientos}</h3>
-          <Reset resetClick={this.resetClick}></Reset>
-        </div>
-      );
+  _getWinner(values){
+    var winner = undefined;
+    var n = values.length;
+    for(let i=0; i<n; i++){
+      for(let j=0; j<n; j++){
+        if(values[i][j] != "-"){
+          var horizontalRight = (n - i > 2);
+          var verticalDown = (n - j > 2);
+          var diagonalRightDown = ((horizontalRight)&&(verticalDown));
+          if(horizontalRight){
+            if((values[i][j]==values[i+1][j])&&(values[i][j]==values[i+2][j])){
+              winner = values[i][j];
+            }
+          }
+          if(verticalDown){
+            if((values[i][j]==values[i][j+1])&&(values[i][j]==values[i][j+2])){
+              winner = values[i][j];
+            }
+          }
+          if(diagonalRightDown){
+            if((values[i][j]==values[i+1][j+1])&&(values[i][j]==values[i+2][j+2])){
+              winner = values[i][j];
+            }
+          }
+        }
+      }
+    }
+    if(typeof winner != "undefined"){
+      if(winner == 'X'){
+        winner = PLAYER_X;
+      } else {
+        winner = PLAYER_0;
+      }
+    }
+    return winner;
   }
 
+  render(){
+    var text = "Turn of " + this.state.turn;
+    return (
+      <div>
+        <Header text={text}/>
+        <Board values={this.state.values} onClick={this._onClick} winner={this.state.winner}/>
+      </div>
+    );
+  }
 }
